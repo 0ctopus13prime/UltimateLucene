bd47b1eab9da0bcc125cdad627d4050edaf8356f
dooyong.kim <dooyong.kim@navercorp.com>
2018-04-30 12:16:35 +0900

    1. Added IndexableField, IndexableFieldType and Field's header file.

---
+++ document/Field.h
@@ -0,0 +1,21 @@
+#ifndef LUCENE_CORE_FIELD_H_
+#define LUCENE_CORE_FIELD_H_
+
+namespace lucene {
+  namespace core {
+    namespace document {
+
+      class Field : public IndexableField {
+        private:
+
+        protected:
+
+        public:
+      };
+
+    }; // End of namespace document
+  }; // End of namespace core
+}; // End of namespace lucene
+
+
+#endif
