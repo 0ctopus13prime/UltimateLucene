5db6b34a2fed18280732e01720b34690ec4be946
0ctopus13prime <0ctopus13prime@gmail.com>
2018-05-08 06:57:15 +0000

    1. TokenStream definition is added

--- src/Analysis/TokenStream.h
+++ src/Analysis/TokenStream.h
@@ -1,10 +1,19 @@
 #ifndef LUCENE_CORE_ANALYSIS_TOKEN_STREAM_H_
 #define LUCENE_CORE_ANALYSIS_TOKEN_STREAM_H_
 
+#include <Util/Attribute.h>
+
 namespace lucene { namespace core { namespace analysis {
 
-class TokenStream {
-
+class TokenStream: public AttributeSource {
+  public:
+    TokenStream();
+    ~TokenStream();
+    TokenStream(const AttributeSource& input);
+    TokenStream(AttributeFactory* factory);
+    virtual bool IncrementToken() = 0;
+    virtual void End();
+    virtual void Reset() = 0;
 };
 
 }}} // End of namespace
