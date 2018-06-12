ec64d0063be791d98efc04e4478f8e9745f59990
0ctopus13prime <0ctopus13prime@gmail.com>
2018-06-12 16:48:06 +0000

    1. ArrayUtil test cases are added

--- src/Util/ArrayUtil.cpp
+++ src/Util/ArrayUtil.cpp
@@ -4,17 +4,19 @@ using namespace lucene::core::util;
 
 // TODO. I'm not sure this is a valid implementation. How can I limit maximum array size?
 uint32_t arrayutil::Oversize(const uint32_t min_target_size, const uint32_t bytes_per_element) {
-  uint32_t extra = (min_target_size >> 3);
-  extra = (extra < 3 ? 3 : extra);
+  uint32_t extra = (min_target_size >> 3); // extra <- min_target_size / 8
+  if(extra < 3) {
+    extra = 3;
+  }
 
   uint32_t new_size = (min_target_size + extra);
   switch(bytes_per_element) {
     case 4:
-      return (new_size + 1) & 0xfffffffe;
+      return (new_size + 1) & 0xfffffffe; // 11111111111111111111111111111110
     case 2:
-      return (new_size + 1) & 0xfffffffc;
+      return (new_size + 1) & 0xfffffffc; // 11111111111111111111111111111100
     case 1:
-      return (new_size + 1) & 0xfffffff8;
+      return (new_size + 1) & 0xfffffff8; // 11111111111111111111111111111000
     case 8:
     default:
       return new_size;
--- src/Util/ArrayUtil.h
+++ src/Util/ArrayUtil.h
@@ -13,7 +13,7 @@ namespace lucene { namespace core { namespace util { namespace arrayutil {
 template <typename T>
 T* CopyOf(const T* original, const uint32_t original_length, const uint32_t new_length) {
   T* copy = new T[new_length];
-  std::memcpy(copy, original, std::min(original_length, new_length));
+  std::memcpy(copy, original, sizeof(T) * std::min(original_length, new_length));
   return copy;
 }
 
@@ -29,28 +29,31 @@ template <typename T>
 std::pair<T*, uint32_t> Grow(const T* array, const uint32_t length, const uint32_t min_size) {
   if(length < min_size) {
     uint32_t new_length = Oversize(min_size, sizeof(T));
-    T* new_array = CopyOf(array, new_length);
+    T* new_array = CopyOf(array, length, new_length);
     return std::pair<T*, uint32_t>(new_array, new_length);
   }
 
   return std::pair<T*, uint32_t>(nullptr, 0);
 }
 
+/**
+ * @param original Source array
+ * @param from Start index in original. Inclusive.
+ * @param end end index in original. Exclusive.
+ **/
 template <typename T>
 T* CopyOfRange(const T* original, const uint32_t from, const uint32_t to) {
+  if(from >= to) throw std::invalid_argument("Range must guarantee from < to, from -> " + std::to_string(from) + ", to -> " + std::to_string(to));
+
   const uint32_t new_length = (to - from);
-  if(new_length < 0) throw std::invalid_argument("Range from > to, " + std::to_string(from) + " > " + std::to_string(to));
-  if(new_length > 0) {
-    T* copy = new T[new_length];
-    std::memcpy(copy, original + from, new_length);
-    return copy;
-  } else {
-    return nullptr;
-  }
+  T* copy = new T[new_length];
+  std::memcpy(copy, original + from, sizeof(T) * new_length);
+  return copy;
 }
 
 template <typename INTEGER1, typename INTEGER2, typename INTEGER3>
 INTEGER1 CheckFromToIndex(INTEGER1 from_index, INTEGER2 to_index, INTEGER3 length) {
+  // Checks if `from_index` belongs [0, to_indx] and `to_indx` <= length
   if(from_index < 0 || from_index > to_index || to_index > length) {
     throw std::invalid_argument("Range [" + std::to_string(from_index) + ", " + std::to_string(to_index) + ") out of bounds for length " + std::to_string(length));
   }
@@ -60,6 +63,7 @@ INTEGER1 CheckFromToIndex(INTEGER1 from_index, INTEGER2 to_index, INTEGER3 lengt
 
 template <typename INTEGER1, typename INTEGER2, typename INTEGER3>
 INTEGER1 CheckFromIndexSize(INTEGER1 from_index, INTEGER2 size, INTEGER3 length) {
+  // Checks if `from_index` in [0, from_index + size] and `from_index` + `size` < = `length`
   int64_t end = from_index + size;
   if(from_index < 0 || from_index > end || end > length) {
     throw std::invalid_argument("Range [" + std::to_string(from_index) + ", " + std::to_string(from_index) + " + " + std::to_string(size) + ") out of bounds for length " + std::to_string(length));
@@ -70,6 +74,7 @@ INTEGER1 CheckFromIndexSize(INTEGER1 from_index, INTEGER2 size, INTEGER3 length)
 
 template <typename INTEGER1, typename INTEGER2>
 INTEGER1 CheckIndex(INTEGER1 index, INTEGER2 length) {
+  // Checks if `index` belongs [0, length) range
   if(index < 0 || index >= length) {
     throw std::invalid_argument("Index " + std::to_string(index) + " out-of-bounds for length " + std::to_string(length));
   }
