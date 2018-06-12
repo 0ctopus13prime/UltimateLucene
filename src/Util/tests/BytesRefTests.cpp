f6bafd72c2c447e766815f753552bb582bb37b6a
0ctopus13prime <0ctopus13prime@gmail.com>
2018-06-12 15:22:50 +0000

    1. Removed naive assert in BytesRefTests.cpp 2. Rewrote BytesRefTests with Google Test

--- src/Util/tests/BytesRefTests.cpp
+++ src/Util/tests/BytesRefTests.cpp
@@ -1,203 +1,203 @@
+#include <gtest/gtest.h>
 #include <assert.h>
 #include <iostream>
 #include <Util/Bytes.h>
 
 using namespace lucene::core::util;
 
-// TODO Change this naive tests to Google test
+TEST(BytesRefTests, BASIC__TEST) {
+  // Basic test
+  std::string name = "doochi";
+  BytesRef bytes_ref(name);
 
-int main() {
-  std::cout << "Start basic BytesRefTests" << std::endl;
+  EXPECT_EQ(0, bytes_ref.offset);
+  EXPECT_EQ(6, bytes_ref.length);
 
-  {
-    std::cout << "  - Basic test" << std::endl;
-    // Basic test
-    std::string name = "doochi";
-    BytesRef bytes_ref(name);
+  std::string got = bytes_ref.UTF8ToString();
+  EXPECT_EQ("doochi", got);
+}
 
-    assert(bytes_ref.offset == 0);
-    assert(bytes_ref.length == 6);
+TEST(BytesRefTest, SHALLOW__COPY__DEEP__COPY) {
+  // Shallow copy, Deep copy
+  std::string str = "doochi";
+  BytesRef bytes_ref1(str);
+  BytesRef shared_bytes_ref2;
+  bytes_ref1.ShallowCopyTo(shared_bytes_ref2); // Shallow copy
 
-    std::string got = bytes_ref.UTF8ToString();
-    assert(got == "doochi");
+  // Shallow case, Change single character at 0 index
+  bytes_ref1.bytes.get()[0] = 'x';
+  EXPECT_EQ('x', shared_bytes_ref2.bytes.get()[0]);
+
+  // Deep copy case, Chage single character at 0 index
+  BytesRef bytes_ref2 = bytes_ref1;
+  bytes_ref1.bytes.get()[0] = 'd';
+  EXPECT_NE(bytes_ref2.bytes.get()[0], bytes_ref1.bytes.get()[0]);
+  EXPECT_EQ('x', bytes_ref2.bytes.get()[0]);
+
+  // operator =, assign
+  BytesRef bytes_ref3 = bytes_ref2; // Deep copy
+  EXPECT_EQ(bytes_ref3, bytes_ref2);
+
+  BytesRef shared_bytes_ref3;
+  bytes_ref3.ShallowCopyTo(shared_bytes_ref3);
+
+  EXPECT_EQ(shared_bytes_ref3, bytes_ref2);
+}
+
+TEST(BytesRefTest, EQ__COMPARE) {
+  // Compare tests
+  std::string str1 = "doochi stupid";
+  std::string str2 = str1 + " suffix";
+  std::string str3 = "ugly doochi";
+
+  BytesRef bytes_ref1(str1);
+  BytesRef shared_bytes_ref1;
+  bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
+  BytesRef bytes_ref2(str1);
+  BytesRef shared_bytes_ref2(bytes_ref2);
+
+  EXPECT_EQ(bytes_ref1, bytes_ref2);
+  EXPECT_EQ(shared_bytes_ref1, bytes_ref2);
+  EXPECT_EQ(bytes_ref1, shared_bytes_ref2);
+  EXPECT_EQ(shared_bytes_ref1, shared_bytes_ref2);
+}
+
+TEST(BytesRefTest, NOT__EQ__TEST) {
+  std::string str1 = "doochi stupid";
+  std::string str2 = str1 + " suffix";
+  std::string str3 = "ugly doochi";
+
+  BytesRef bytes_ref1(str1);
+  BytesRef shared_bytes_ref1;
+  bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
+
+  BytesRef bytes_ref2(str2);
+  BytesRef shared_bytes_ref2;
+  bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
+
+  EXPECT_NE(bytes_ref1, bytes_ref2);
+  EXPECT_NE(bytes_ref1, shared_bytes_ref2);
+  EXPECT_NE(shared_bytes_ref1, bytes_ref2);
+  EXPECT_NE(shared_bytes_ref1, shared_bytes_ref2);
+}
+
+TEST(BytesRefTest, LESS__THEN__TEST) {
+  std::string str1 = "doochi stupid";
+  std::string str2 = str1 + " suffix";
+  std::string str3 = "ugly doochi";
+
+  BytesRef bytes_ref1(str1);
+  BytesRef shared_bytes_ref1;
+  bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
+
+  BytesRef bytes_ref2(str2);
+  BytesRef shared_bytes_ref2;
+  bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
+
+  EXPECT_LT(bytes_ref1, bytes_ref2);
+  EXPECT_LT(bytes_ref1, shared_bytes_ref2);
+  EXPECT_LT(shared_bytes_ref1, bytes_ref2);
+  EXPECT_LT(shared_bytes_ref1, shared_bytes_ref2);
+
+  BytesRef bytes_ref3(str3);
+  BytesRef shared_bytes_ref3(bytes_ref3);
+  EXPECT_LT(bytes_ref1, bytes_ref3);
+  EXPECT_LT(bytes_ref1, shared_bytes_ref3);
+  EXPECT_LT(shared_bytes_ref1, bytes_ref3);
+  EXPECT_LT(shared_bytes_ref1, shared_bytes_ref3);
+}
+
+TEST(BytesRefTest, LESS__OR__EQ__TEST) {
+  std::string str1 = "doochi stupid";
+  std::string str2 = str1 + " suffix";
+  std::string str3 = "ugly doochi";
+
+  BytesRef bytes_ref1(str1);
+  BytesRef shared_bytes_ref1;
+  bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
+
+  BytesRef bytes_ref2(str2);
+  BytesRef shared_bytes_ref2;
+  bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
+
+  EXPECT_LE(bytes_ref1, bytes_ref2);
+  EXPECT_LE(bytes_ref1, shared_bytes_ref2);
+  EXPECT_LE(shared_bytes_ref1, bytes_ref2);
+  EXPECT_LE(shared_bytes_ref1, shared_bytes_ref2);
+
+  BytesRef bytes_ref3(str3);
+  BytesRef shared_bytes_ref3(bytes_ref3);
+  EXPECT_LE(bytes_ref1, bytes_ref3);
+  EXPECT_LE(bytes_ref1, shared_bytes_ref3);
+  EXPECT_LE(shared_bytes_ref1, bytes_ref3);
+  EXPECT_LE(shared_bytes_ref1, shared_bytes_ref3);
+}
+
+TEST(BytesRefTest, GREATER__THEN__TEST) {
+  std::string str1 = "doochi stupid";
+  std::string str2 = str1 + " suffix";
+  std::string str3 = "ugly doochi";
+
+  BytesRef bytes_ref1(str1);
+  BytesRef shared_bytes_ref1;
+  bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
+
+  BytesRef bytes_ref2(str2);
+  BytesRef shared_bytes_ref2;
+  bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
+
+  EXPECT_GT(bytes_ref2, bytes_ref1);
+  EXPECT_GT(bytes_ref2, shared_bytes_ref1);
+  EXPECT_GT(shared_bytes_ref2, bytes_ref1);
+  EXPECT_GT(shared_bytes_ref2, shared_bytes_ref1);
+
+  BytesRef bytes_ref3(str3);
+  BytesRef shared_bytes_ref3(bytes_ref3);
+  EXPECT_GT(bytes_ref3, bytes_ref1);
+  EXPECT_GT(bytes_ref3, shared_bytes_ref1);
+  EXPECT_GT(shared_bytes_ref3, bytes_ref1);
+  EXPECT_GT(shared_bytes_ref3, shared_bytes_ref1);
+}
+
+TEST(BytesRefTest, GREATER__OR__EQ__TEST) {
+  std::string str1 = "doochi stupid";
+  std::string str2 = str1 + " suffix";
+  std::string str3 = "ugly doochi";
+
+  BytesRef bytes_ref1(str1);
+  BytesRef shared_bytes_ref1;
+  bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
+
+  BytesRef bytes_ref2(str2);
+  BytesRef shared_bytes_ref2;
+  bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
+
+  EXPECT_GE(bytes_ref2, bytes_ref1);
+  EXPECT_GE(bytes_ref2, shared_bytes_ref1);
+  EXPECT_GE(shared_bytes_ref2, bytes_ref1);
+  EXPECT_GE(shared_bytes_ref2, shared_bytes_ref1);
+
+  BytesRef bytes_ref3(str3);
+  BytesRef shared_bytes_ref3(bytes_ref3);
+  EXPECT_GE(bytes_ref3, bytes_ref1);
+  EXPECT_GE(bytes_ref3, shared_bytes_ref1);
+  EXPECT_GE(shared_bytes_ref3, bytes_ref1);
+  EXPECT_GE(shared_bytes_ref3, shared_bytes_ref1);
+}
+
+TEST(BytesRefTest, VALIDATION__TEST) {
+  BytesRef bytes_ref;
+  bytes_ref.offset = 1; // nullptr && offset > length
+  try {
+    bytes_ref.IsValid();
+    assert(false); // Fail here
+  } catch(...) {
+    // Ignore, Exception must be caught in here.
   }
+}
 
-  {
-    std::cout << "  - Shallow copy, Deep copy" << std::endl;
-    // Shallow copy, Deep copy
-    std::string str = "doochi";
-    BytesRef bytes_ref1(str);
-    BytesRef shared_bytes_ref2;
-    bytes_ref1.ShallowCopyTo(shared_bytes_ref2); // Shallow copy
 
-    // Shallow case, Change single character at 0 index
-    bytes_ref1.bytes.get()[0] = 'x';
-    assert(shared_bytes_ref2.bytes.get()[0] == 'x');
-
-    // Deep copy case, Chage single character at 0 index
-    BytesRef bytes_ref2 = bytes_ref1;
-    bytes_ref1.bytes.get()[0] = 'd';
-    assert(bytes_ref2.bytes.get()[0] != bytes_ref1.bytes.get()[0]);
-    assert(bytes_ref2.bytes.get()[0] == 'x');
-
-    // operator =, assign
-    BytesRef bytes_ref3 = bytes_ref2; // Deep copy
-    assert(bytes_ref3 == bytes_ref2);
-
-    BytesRef shared_bytes_ref3;
-    bytes_ref3.ShallowCopyTo(shared_bytes_ref3);
-
-    assert(shared_bytes_ref3 == bytes_ref2);
-  }
-
-  {
-    std::cout << "  - Compare tests" << std::endl;
-    // Compare tests
-    std::string str1 = "doochi stupid";
-    std::string str2 = str1 + " suffix";
-    std::string str3 = "ugly doochi";
-
-    // 1. ==
-    {
-      std::cout << "    - == test" << std::endl;
-      BytesRef bytes_ref1(str1);
-      BytesRef shared_bytes_ref1;
-      bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
-      BytesRef bytes_ref2(str1);
-      BytesRef shared_bytes_ref2(bytes_ref2);
-
-      assert(bytes_ref1 == bytes_ref2);
-      assert(shared_bytes_ref1 == bytes_ref2);
-      assert(bytes_ref1 == shared_bytes_ref2);
-      assert(shared_bytes_ref1 == shared_bytes_ref2);
-    }
-
-    // 2. !=
-    {
-      std::cout << "    - != test" << std::endl;
-      BytesRef bytes_ref1(str1);
-      BytesRef shared_bytes_ref1;
-      bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
-
-      BytesRef bytes_ref2(str2);
-      BytesRef shared_bytes_ref2;
-      bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
-
-      assert(bytes_ref1 != bytes_ref2);
-      assert(bytes_ref1 != shared_bytes_ref2);
-      assert(shared_bytes_ref1 != bytes_ref2);
-      assert(shared_bytes_ref1 != shared_bytes_ref2);
-    }
-
-    // 3. <
-    {
-      std::cout << "    - < test" << std::endl;
-      BytesRef bytes_ref1(str1);
-      BytesRef shared_bytes_ref1;
-      bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
-
-      BytesRef bytes_ref2(str2);
-      BytesRef shared_bytes_ref2;
-      bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
-
-      assert(bytes_ref1 < bytes_ref2);
-      assert(bytes_ref1 < shared_bytes_ref2);
-      assert(shared_bytes_ref1 < bytes_ref2);
-      assert(shared_bytes_ref1 < shared_bytes_ref2);
-
-      BytesRef bytes_ref3(str3);
-      BytesRef shared_bytes_ref3(bytes_ref3);
-      assert(bytes_ref1 < bytes_ref3);
-      assert(bytes_ref1 < shared_bytes_ref3);
-      assert(shared_bytes_ref1 < bytes_ref3);
-      assert(shared_bytes_ref1 < shared_bytes_ref3);
-    }
-
-    // 4. <=
-    {
-      std::cout << "    - <= test" << std::endl;
-      BytesRef bytes_ref1(str1);
-      BytesRef shared_bytes_ref1;
-      bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
-
-      BytesRef bytes_ref2(str2);
-      BytesRef shared_bytes_ref2;
-      bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
-
-      assert(bytes_ref1 <= bytes_ref2);
-      assert(bytes_ref1 <= shared_bytes_ref2);
-      assert(shared_bytes_ref1 <= bytes_ref2);
-      assert(shared_bytes_ref1 <= shared_bytes_ref2);
-
-      BytesRef bytes_ref3(str3);
-      BytesRef shared_bytes_ref3(bytes_ref3);
-      assert(bytes_ref1 <= bytes_ref3);
-      assert(bytes_ref1 <= shared_bytes_ref3);
-      assert(shared_bytes_ref1 <= bytes_ref3);
-      assert(shared_bytes_ref1 <= shared_bytes_ref3);
-    }
-
-    // 5. >
-    {
-      std::cout << "    - > test" << std::endl;
-      BytesRef bytes_ref1(str1);
-      BytesRef shared_bytes_ref1;
-      bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
-
-      BytesRef bytes_ref2(str2);
-      BytesRef shared_bytes_ref2;
-      bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
-
-      assert(bytes_ref2 > bytes_ref1);
-      assert(bytes_ref2 > shared_bytes_ref1);
-      assert(shared_bytes_ref2 > bytes_ref1);
-      assert(shared_bytes_ref2 > shared_bytes_ref1);
-
-      BytesRef bytes_ref3(str3);
-      BytesRef shared_bytes_ref3(bytes_ref3);
-      assert(bytes_ref3 > bytes_ref1);
-      assert(bytes_ref3 > shared_bytes_ref1);
-      assert(shared_bytes_ref3 > bytes_ref1);
-      assert(shared_bytes_ref3 > shared_bytes_ref1);
-    }
-
-    // 6. >=
-    {
-      std::cout << "    - >= test" << std::endl;
-      BytesRef bytes_ref1(str1);
-      BytesRef shared_bytes_ref1;
-      bytes_ref1.ShallowCopyTo(shared_bytes_ref1);
-
-      BytesRef bytes_ref2(str2);
-      BytesRef shared_bytes_ref2;
-      bytes_ref2.ShallowCopyTo(shared_bytes_ref2);
-
-      assert(bytes_ref2 >= bytes_ref1);
-      assert(bytes_ref2 >= shared_bytes_ref1);
-      assert(shared_bytes_ref2 >= bytes_ref1);
-      assert(shared_bytes_ref2 >= shared_bytes_ref1);
-
-      BytesRef bytes_ref3(str3);
-      BytesRef shared_bytes_ref3(bytes_ref3);
-      assert(bytes_ref3 >= bytes_ref1);
-      assert(bytes_ref3 >= shared_bytes_ref1);
-      assert(shared_bytes_ref3 >= bytes_ref1);
-      assert(shared_bytes_ref3 >= shared_bytes_ref1);
-    }
-  }
-
-  {
-    std::cout << "  - Validation test" << std::endl;
-    // Validation test
-    BytesRef bytes_ref;
-    bytes_ref.offset = 1; // nullptr && offset > length
-    try {
-      bytes_ref.IsValid();
-      assert(false); // Fail here
-    } catch(...) {
-      // Ignore, Exception must be caught in here.
-    }
-  }
-
-  std::cout << "End basic BytesRefTests" << std::endl;
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
